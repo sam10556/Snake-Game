fe3cdae9205e6c784bb3d7b7a435be6f6cdea581
Samyak <samyak.10556@gmail.com>
2024-01-30 22:44:21 +0530

    updated all the files

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+//Define HTML elem
+const board = document.getElementById('game-board');
+
+
+let snake = [{x:10, y:10}];
+
+
+//draw snake,map,fruit
+function draw(){
+    board.innerHTML = '';
+    drawSnake();
+}
+
+
+function drawSnake(){
+
+}
